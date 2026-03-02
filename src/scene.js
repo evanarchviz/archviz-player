d9ce9c1cb5e1576eb4be15e41053eea342f14627
evanarchviz <evanarchviz@gmail.com>
2026-03-02 10:27:51 +0200

    Update scene.js

--- src/scene.js
+++ src/scene.js
@@ -1,8 +1,11 @@
-function loadScene() {
+function loadScene(){
 
     const loader = new THREE.GLTFLoader();
-    loader.load("assets/scene.glb", function (gltf) {
+
+    loader.load("assets/scene.glb", function(gltf){
+
         window.app.scene.add(gltf.scene);
+
         initPlayer();
         animate();
     });
