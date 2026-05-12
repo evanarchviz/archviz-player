4de715e9d46d002fbb29b938eb12b8e28f09fdaf
evanarchviz <evanarchviz@gmail.com>
2026-05-12 04:12:30 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const speed = 4.5;
 const stepHeight = 0.2;
 let playerBaseY = 0;
 
-const SPAWN = new THREE.Vector3(0, 0, 0);
+const SPAWN = new THREE.Vector3(-8.7799, 6.67481, 12.5123);
 
 init();
 
